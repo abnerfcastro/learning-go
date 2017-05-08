d2b0e6f5343d6879ebf6826d3ae1ae414a2f153f
Abner Castro <abnerfcastro@gmail.com>
2017-05-07 23:58:34 -0300

    learning config files

---
+++ config-files/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/vharitonsky/iniflags"
+)
+
+func main() {
+
+	// flags to be read from dev.ini
+	greetingPtr := flag.String("greeting", "Hello", "This is how the user will be greeted.")
+	treatmentPtr := flag.String("treatment", "Mr.", "This is the title the user will be addressed with.")
+	namePtr := flag.String("name", "Abner Castro", "This is the name of the user")
+
+	iniflags.Parse()
+
+	fmt.Println(*greetingPtr, *treatmentPtr, *namePtr)
+
+}
