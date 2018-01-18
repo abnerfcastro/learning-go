56fc67ba0cb784301a6950c305774fbcf2f5d72e
Abner Castro <abnerfcastro@gmail.com>
2018-01-18 18:50:10 +0000

    Test commit from c9

--- basics/variables.go
+++ basics/variables.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	fmt.Printf("Program name: %s\n", os.Args[0])
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
